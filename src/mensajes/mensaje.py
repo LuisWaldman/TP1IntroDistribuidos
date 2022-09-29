d61d97af19651047231e9070563f747f116325c0
lwaldman1 <luiswaldman@gmail.com>
2022-09-29 20:24:49 -0300

    mas mensajes

--- src/mensajes/mensaje.py
+++ src/mensajes/mensaje.py
@@ -5,8 +5,9 @@ class TipoMensaje(enum.Enum):
     NODEFINIDO = 0
     HOLA = 1
     CHAU = 2
-    ACK = 3
-    ERROR = 4
+    PARTE = 3
+    ACK = 4
+    ERROR = 5
 class EnumOperacion(enum.Enum):
     DOWNLOAD = 1
     UPLOAD = 2
@@ -21,9 +22,23 @@ class Mensaje:
 class Mensaje_hola(Mensaje):
     operacion = EnumOperacion.UPLOAD
     protocolo = EnumProtocolo.GBN
+    archivo = ""
 
-    def __init__(self, p_operacion, p_protocolo):
+    def __init__(self, p_operacion, p_protocolo, p_archivo):
         self.tipo = TipoMensaje.HOLA
         self.operacion = p_operacion
         self.protocolo = p_protocolo
+        self.archivo = p_archivo
 
+
+
+class Mensaje_parte(Mensaje):
+    id_conexion = 0
+    parte = 0
+    totalpartes = 0
+
+
+class Mensaje_ack(Mensaje):
+    id_conexion = 0
+    parte = 0
+    totalpartes = 0
