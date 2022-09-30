acc938941af26fa9b960142f5559eb523f8fb604
lwaldman1 <luiswaldman@gmail.com>
2022-09-29 21:19:05 -0300

    Download stop & wait completo

--- src/download.py
+++ src/download.py
@@ -4,6 +4,10 @@ from src.utils.parametros import *
 from socket import *
 from src.mensajes.mensaje import *
 from src.utils.Traductor import *
+from src.utils.fragmentador import Fragmentador
+from src.utils.desfragmentador import Desfragmentador
+
+
 
 param = Parametros(sys.argv)
 if param.mostrar_ayuda:
@@ -30,11 +34,36 @@ salida.verborragica("path:"+ str(param.path))
 salida.verborragica("filename:" + str(param.filename))
 
 
+salida.verborragica("Inicio Parametros")
+mss = 100
+
+
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+salida.info("Iniciando comunicacion STOP & WAIT")
 primermensaje = Mensaje_hola(EnumOperacion.DOWNLOAD, EnumProtocolo.STOPANDWAIT)
-message = Traductor.MensajeAPaquete(primermensaje)
-clientSocket.sendto(message, (param.ip, param.port))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-salida.info(modifiedMessage.decode())
+primerpaquete = Traductor.MensajeAPaquete(primermensaje)
+clientSocket.sendto(primerpaquete, (param.ip, param.port))
+terminoarhivo = False
+
+desfrag = Desfragmentador(param.filename, mss)
+while not terminoarhivo:
+    salida.verborragica("esperando paquete ...")
+    paqueterecibido, serverAddress = clientSocket.recvfrom(2048)
+    salida.verborragica("paquete recibido")
+    mensajerecibido = Traductor.PaqueteAMensaje(paqueterecibido)
+
+    aux = desfrag.set_bytes_to_file(mensajerecibido.partearchivo, mensajerecibido.parte)
+    salida.verborragica("Bytes Escritos" + aux)
+
+    mensajeAck = Mensaje_ack(mensajerecibido.parte)
+    paqueteack = Traductor.MensajeAPaquete(mensajeAck)
+    salida.verborragica("Envia ACK parte" + mensajerecibido.parte)
+    clientSocket.sendto(paqueteack, (param.ip, param.port))
+    if mensajerecibido.parte == mensajerecibido.totalpartes:
+        terminoarhivo = True
+
+
+
 clientSocket.close()
+salida.info("comunicacion terminada")
--- src/pruebas/pruebatraductormensajes.py
+++ src/pruebas/pruebatraductormensajes.py
@@ -6,7 +6,7 @@ prot = EnumProtocolo.GBN
 
 mensaje = Mensaje_hola(oper, prot)
 paquete = Traductor.MensajeAPaquete(mensaje)
-mensaje_retraducido = Traductor.PaqueteAMensaje(paquete);
+mensaje_retraducido = Traductor.PaqueteAMensaje(paquete)
 assert mensaje_retraducido.tipo == TipoMensaje.HOLA
 assert mensaje_retraducido.protocolo == prot
 assert mensaje_retraducido.operacion == oper
