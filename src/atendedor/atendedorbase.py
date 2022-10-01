cb5636347ac7f3906142ac96ee2f8e307db36762
lwaldman1 <luiswaldman@gmail.com>
2022-10-01 14:46:10 -0300

    Agrego los distintos atendedores

---
+++ src/atendedor/atendedorbase.py
@@ -0,0 +1,16 @@
+class AtendedorBase:
+    direccion = ""
+    N = 1
+    def __init__(self, pdireccion, pN):
+        self.direccion = pdireccion
+        self.N = pN
+
+    def LlegoPaquete(Paquete):
+        pa = Paquete
+
+    def TimeOut(self):
+        a = 1
+
+
+
+
